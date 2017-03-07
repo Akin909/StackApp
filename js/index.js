33ca39027a5bc3725d0ca529c3f89aa1ec225386
antoniotrkdz <antoniotrkdz@gmail.com>
2017-03-07 16:36:55 +0000

    kinda working

--- js/index.js
+++ js/index.js
@@ -1,20 +1,24 @@
 var queryString;
 
 
-var makeRequest = function(url) {
+var makeRequest = function(url,cb) {
     var xhr = new XMLHttpRequest;
     xhr.onreadystatechange = function() {
         if (xhr.status === 200 && xhr.readyState === 4) {
             var responseObject = JSON.parse(xhr.response);
-            console.log(responseObject);
-            return responseObject;
+            // console.log(responseObject);
+            cb(responseObject);
         }
-    }
-
+      }
     xhr.open('GET', url, true);
     xhr.send();
 }
 
+function cb (response) {
+  console.log(response);
+  renderToDOM(response,'results-body')
+  //  return response;
+ }
 
 function getInput(id) {
     var input = document.getElementById(id)
@@ -29,14 +33,14 @@ function queryToTags(queryString) {
 
 //This function takes our user's input and returns it (it is used above)
 
-function submitSearch() {
+(function submitSearch() {
     var input = getInput("inputText");
     input.addEventListener('keyup', function(event) {
         if (event.keyCode === 13) {
             console.log(input.value);
             queryString = input.value;
             var request = urlConstructor(input.value)
-            makeRequest(request);
+            makeRequest(request,cb);
             return input.value
         }
     })
@@ -45,21 +49,26 @@ function submitSearch() {
         console.log(input.value);
         queryString = input.value;
         var request = urlConstructor(input.value)
-        makeRequest(request);
+        makeRequest(request,cb);
         return input.value
     })
 
-}
+})();
 
 
 function urlConstructor(usersQuery) {
     console.log('usersQuery',usersQuery);
     var submission = queryToTags(usersQuery);
     var access_key = 'UcfoxNCNMRw57sKFF83MgQ(('
-    var url = 'https://api.stackexchange.com/docs/questions/unanswered?tagged=' + submission + '&site=stackoverflow';
+    var url = 'https://api.stackexchange.com/2.2/questions/unanswered?tagged=' + submission + '&site=stackoverflow&key=' + access_key;
     return url;
 }
-console.log(makeRequest(urlConstructor('javascript array')));
-console.log(urlConstructor('javascrip'));
+// console.log(makeRequest(urlConstructor('javascript array'),cb));
+// console.log(urlConstructor('javascrip'));
 // var javasScript = urlConstructor()
 // makeRequest();
+
+function renderToDOM (result,id) {
+  document.getElementById(id).innerText = result.items[2].title;
+      // console.log(el);
+}
